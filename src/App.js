18a03bcdb170093f3a8a09a18bb759052334a15e
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-18 18:22:18 +0800

    childs and sub childs done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // components
 import Parent1 from './components/Parent1/Parent1';
 import Parent2 from './components/Parent2/Parent2';
+import Child1P1 from './components/Parent1/Child1P1/Child1P1';
+import Child1P2 from './components/Parent2/Child1P2/Child1P2';
+import Sub1Child1P1 from './components/Parent1/Child1P1/SubChild1P1/Sub1Child1P1';
+import Sub2Child1P1 from './components/Parent1/Child1P1/SubChild1P1/Sub2Child1P1';
 
 // state
 import { useStateValue } from './state/StateProvider';
 
-
 function App() {
-  // const [user, setUser] = useState(null);
-  const [{ user, age }, dispatch] = useStateValue();
+  const [{ user, age }] = useStateValue();
 
   return (
-    <div className="app">
+    <main>
       <div className="outputSection">
         <h1 style={{}}>Output</h1>
-        <p>User: {user}</p>
-        <p>Age: {age}</p>
+        <p><strong>User:  </strong>{user}</p>
+        <p><strong>Age:  </strong>{age}</p>
       </div>
 
       <div className="inputSection">
-        <h1 className="title">Context Api</h1>
-        <br />
-        <Parent1 />
-        <br />
-        <Parent2 />
+        <h1 className="title">Context Api</h1><br />
+        <Parent1 /><br />
+        <Parent2 /><br />
+        <Child1P1 /><br />
+        <Child1P2 /><br />
+        <Sub1Child1P1 /><br />
+        <Sub2Child1P1 />
       </div>
-    </div>
+    </main>
   );
 }
 
---
+++ src/components/Parent1/Child1P1/Child1P1.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+// state
+import { useStateValue } from '../../../state/StateProvider';
+
+const Child1P1 = ({ setUser }) => {
+    const [{ user, age }, dispatch] = useStateValue();
+    
+    // ADD user
+    const addUser = () => {
+        dispatch({
+            type: 'SET_USER',
+            user: 'Md Arif Hossain'
+        })
+    };
+
+    // REMOVE user
+    const removeUser = () => {
+        dispatch({
+            type: 'REMOVE_USER'
+        })
+    };
+
+    // UPDATE user
+    const onChangeUserInput = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: 'SET_USER',
+            user: e.target.value
+        })
+    };
+
+    // ADD age
+    const addAge = () => {
+        dispatch({
+            type: 'SET_AGE',
+            age: '21'
+        })
+    };
+
+    // REMOVE age
+    const removeAge = () => {
+        dispatch({
+            type: 'REMOVE_AGE'
+        })
+    };
+
+    // UPDATE age
+    const onChangeAgeInput = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: 'SET_AGE',
+            age: e.target.value
+        })
+    };
+
+    return (
+        <section>
+            <h1>Child1 Of Parent1</h1>
+            <button onClick={addUser}>Set user Arif</button>
+            <button onClick={removeUser}>Remove user</button>
+
+            <br /><br />
+            <button onClick={addAge}>Set age 21</button>
+            <button onClick={removeAge}>Remove age</button>
+
+            <br /><br />
+            <label htmlFor="user">Update user</label>
+            <input name="user" onChange={e => onChangeUserInput(e)} value={user ? user : ''} />
+
+            <br /><br />
+            <label htmlFor="age">Update age</label>
+            <input name="age" onChange={e => onChangeAgeInput(e)} value={age ? age : ''} />
+        </section>
+    )
+}
+
+export default Child1P1;
