ef35067dcf98155422cdff372020068c6da38cd7
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-20 20:14:11 +0800

    Updates

---
+++ precache-manifest.670e215b9307723855a6988a91713fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b669264c582d75645c056b1407d8e213",
+    "url": "/context-api-template/index.html"
+  },
+  {
+    "revision": "1296b7a575623b1f3bad",
+    "url": "/context-api-template/static/css/main.65129250.chunk.css"
+  },
+  {
+    "revision": "1b1ca3ca4e1c468fb7d8",
+    "url": "/context-api-template/static/js/2.d5d4acbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/context-api-template/static/js/2.d5d4acbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1296b7a575623b1f3bad",
+    "url": "/context-api-template/static/js/main.c8e1704c.chunk.js"
+  },
+  {
+    "revision": "310af50760e4c8608b0b",
+    "url": "/context-api-template/static/js/runtime-main.5b098caf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11364491d8885814bc1d28eb1908ef34.js"
+  "/context-api-template/precache-manifest.670e215b9307723855a6988a91713fc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/context-api-template/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
